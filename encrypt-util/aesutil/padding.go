e77723dd54f3b0764d504dc961e402070642dc2c
康茜 <xikang2@creditease.cn>
2021-08-06 18:08:36 +0800

    add pkcs7 padding

--- encrypt-util/aesutil/padding.go
+++ encrypt-util/aesutil/padding.go
@@ -5,7 +5,13 @@ import (
 	"crypto/aes"
 )
 
-//一些跟aes加密相关的数据填充方法
+/*
+	一些跟aes加密相关的数据填充方法
+
+	pkcs7填充算法: 根据blockSize计算填充所需要的长度size，然用size所表示的ASCII码(ch)对数据进行填充。
+	pkcs5填充算法: 根据固定blockSize=8计算需要填充的长度size，然后使用size 所表示的ASCII码(ch)对数据进行填充。
+	tips：PKCS7与PKCS5的区别在于PKCS5只填充到8字节，而PKCS7可以在1-255之间任意填充。
+*/
 type PaddingFunc func(origin []byte, blockSize int) []byte
 type UnPaddingFunc func(cipher []byte) []byte
 
@@ -42,4 +48,15 @@ func ZeroUnPadding(cipherData []byte) []byte {
 		func(r rune) bool {
 			return r == rune(0)
 		})
+}
+
+func PKCS7Padding(origin []byte, blockSize int) []byte {
+	padding := blockSize - len(origin)%blockSize
+	padText := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(origin, padText...)
+}
+func PKCS7UnPadding(cipher []byte) []byte {
+	l := len(cipher)
+	unpadding := int(cipher[l-1])
+	return cipher[:l-unpadding]
 }
